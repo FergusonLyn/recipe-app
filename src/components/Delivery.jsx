ad6a0fc800af11edb4cf7c4f3fc0ed679fd6cadc
FergusonLyn <psky730@gmail.com>
2023-12-03 20:25:30 +0800

    a recipe project

--- src/components/Delivery.jsx
+++ src/components/Delivery.jsx
@@ -4,9 +4,10 @@ const Delivery = () => {
   return (
     <div className='w-full bg-white py-16 px-4'>
         <h3 className='text-orange-500 font-bold text-2xl text-center'>Quick Delivery App</h3>
-        <div className='w-[1240px] mx-auto grid md:grid-cols-2'>
-            <img src="https://www.slerp.com/wp-content/themes/slerp-theme/assets/images/page-apps-image-3.webp" alt="" className='w-[550px] mx-auto my-4' />
-            <div className='flex flex-col justify-center'>
+        <div className='max-w-full mx-auto grid md:grid-cols-2'>
+        <img src="https://www.slerp.com/wp-content/themes/slerp-theme/assets/images/page-apps-image-3.webp" alt="" className='w-full md:w-[550px] mx-auto my-4' />
+
+            <div className='flex flex-col justify-center md:text-left md:items-start'>
                 <p className='text-[#00df9a] font-bold'>Get The app</p>
                 <h1 className='md:text-4xl sm:text-3xl text-2xl font-bold py-2'>Limitless Convenience on-demand</h1>
                 <p>
